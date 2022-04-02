5560da532ada8da5d7bbd08e7525f3c61c9ec43c
sonalishelkar <98012257+sonalishelkar@users.noreply.github.com>
2022-04-02 20:48:10 +0530

    Factorial Numbers

---
+++ assign9.java
@@ -0,0 +1,20 @@
+
+//Java Program to Find Factorial of a Numbers
+
+import java.util.*;
+public class assign9
+{
+     public static void main(String []args)
+     {
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println("Enter the number: ");
+        int num=sc.nextInt();
+        int fact=1;
+        for(int i=1;i<=num;i++)
+        {
+            fact=fact*i;
+        }
+        System.out.println("Factorial of the number: "+fact); 
+     }  
+}
